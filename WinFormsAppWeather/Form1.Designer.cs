26c9dd446a2fa7ba22fbf331b091e6e413fcfd7c
GenkoKsenia <froglovesmushrooms@mail.ru>
2024-06-03 16:13:26 +0800

    Добавьте файлы проекта.

---
+++ WinFormsAppWeather/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WinFormsAppWeather
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRefill = new Button();
+            txtInfo = new RichTextBox();
+            txtOut = new RichTextBox();
+            btnGet = new Button();
+            SuspendLayout();
+            // 
+            // btnRefill
+            // 
+            btnRefill.Font = new Font("Segoe UI", 12F);
+            btnRefill.Location = new Point(13, 12);
+            btnRefill.Name = "btnRefill";
+            btnRefill.Size = new Size(307, 33);
+            btnRefill.TabIndex = 0;
+            btnRefill.Text = "Перезаполнить";
+            btnRefill.UseVisualStyleBackColor = true;
+            btnRefill.Click += btnRefill_Click;
+            // 
+            // txtInfo
+            // 
+            txtInfo.BorderStyle = BorderStyle.None;
+            txtInfo.Font = new Font("Segoe UI", 12F);
+            txtInfo.Location = new Point(13, 51);
+            txtInfo.Name = "txtInfo";
+            txtInfo.ReadOnly = true;
+            txtInfo.Size = new Size(276, 46);
+            txtInfo.TabIndex = 1;
+            txtInfo.Text = "";
+            // 
+            // txtOut
+            // 
+            txtOut.Font = new Font("Segoe UI", 12F);
+            txtOut.Location = new Point(13, 103);
+            txtOut.Name = "txtOut";
+            txtOut.Size = new Size(226, 118);
+            txtOut.TabIndex = 2;
+            txtOut.Text = "";
+            // 
+            // btnGet
+            // 
+            btnGet.Font = new Font("Segoe UI", 12F);
+            btnGet.Location = new Point(245, 103);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(75, 118);
+            btnGet.TabIndex = 3;
+            btnGet.Text = "Взять";
+            btnGet.UseVisualStyleBackColor = true;
+            btnGet.Click += btnGet_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(332, 233);
+            Controls.Add(btnGet);
+            Controls.Add(txtOut);
+            Controls.Add(txtInfo);
+            Controls.Add(btnRefill);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Прогноз погоды";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRefill;
+        private RichTextBox txtInfo;
+        private RichTextBox txtOut;
+        private Button btnGet;
+    }
+}
---
+++ WinFormsAppWeather/Weather.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WinFormsAppWeather
+{
+    public class Weather
+    {
+        public static Random rnd = new Random();
+
+        public int Temperature = 0;
+        public virtual String GetInfo()
+        {
+            var str = String.Format("\nТемпература: {0}", this.Temperature);
+            return str;
+        }
+    }
+    public enum TF { присутствует, отсутствует };
+    public class Sun : Weather
+    {        
+        public int HeightHorizon = 0; // высота над горизонтом
+        public TF Breeze = TF.отсутствует; // наличие ветерка
+
+        public override String GetInfo()
+        {
+            var str = "Солнечная погода";
+            str += base.GetInfo();
+            str += String.Format("\nСолнышко поднялось на: {0}", this.HeightHorizon);
+            str += String.Format("\nВетерок: {0}", this.Breeze);
+            return str;
+        }
+        public static Sun Generate()
+        {          
+            return new Sun
+            {
+                Temperature = -40 + rnd.Next() % 81, // температура от -40 до 40
+                HeightHorizon = 20 + rnd.Next() % 50, // высота солнца от 20 до 50
+                Breeze = (TF)rnd.Next(2) // наличие ветерка true или false
+            };
+        }
+    }
+
+    public class Rain : Weather
+    {
+        public int Precipitation = 0; // осадки
+        public TF Rainbow = TF.отсутствует; // наличие радуги
+        public TF Thunderstorm = TF.отсутствует; // наличие грозы
+
+        public override String GetInfo()
+        {
+            var str = "За окном дождик";
+            str += base.GetInfo();
+            str += String.Format("\nОсадки: {0}", this.Precipitation);
+            str += String.Format("\nРадуга: {0}", this.Rainbow);
+            str += String.Format("\nГроза: {0}", this.Thunderstorm);
+            return str;
+        }
+        public static Rain Generate()
+        {
+            return new Rain
+            {
+                Temperature = -40 + rnd.Next() % 81, // температура от -40 до 40
+                Precipitation = 1 + rnd.Next() % 50, // осадки от 1 до 50
+                Rainbow = (TF)rnd.Next(2), // наличие радуги true или false
+                Thunderstorm = (TF)rnd.Next(2) // наличие грозы true или false
+            };
+        }
+    }
+
+    public enum Type { мелкий, хлопьями, мокрый};
+    public class Snow : Weather
+    {
+        public Type SnowType = Type.мелкий; // тип снега
+        public int HeightSnowdrift = 0; // высота сугроба
+
+        public override String GetInfo()
+        {            
+            var str = "Идет снег";
+            str += base.GetInfo();
+            str += String.Format("\nТип снега: {0}", this.SnowType);
+            str += String.Format("\nВысота сугроба: {0}", this.HeightSnowdrift);
+            return str;
+        }
+        public static Snow Generate()
+        {
+            return new Snow
+            {
+                Temperature = -40 + rnd.Next() % 41, // температура от -40 до 40
+                SnowType = (Type)rnd.Next(2), //тип снега
+                HeightSnowdrift = rnd.Next() % 10 // высота сугроба от 0 до 10
+            };
+        }
+    }    
+}
